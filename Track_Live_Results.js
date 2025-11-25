dcc008765867eca977ef79d0fffd3a1b2ebdb1c1
Phil-Johnson-4 <151374277+Phil-Johnson-4@users.noreply.github.com>
2025-11-25 09:58:22 -0600

    TEST B

--- Track_Live_Results.js
+++ Track_Live_Results.js
@@ -3,8 +3,8 @@
 //const service = await server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb');
 //const characteristic = await service.getCharacteristic('0000fff1-0000-1000-8000-00805f9b34fb');
 // NEW CODE WITH THE UPDATED UUID ADDRESS? ORIGINAL CODE COMMENTED OUT UP ABOVE.
-const service = await server.getPrimaryService('3ab10100-f831-4395-b29d-570977d5bf94');
-const characteristic = await service.getCharacteristic('3ab10100-f831-4395-b29d-570977d5bf94');
+//const service = await server.getPrimaryService('3ab10100-f831-4395-b29d-570977d5bf94');
+//const characteristic = await service.getCharacteristic('3ab10100-f831-4395-b29d-570977d5bf94');
 
 await characteristic.startNotifications();
 //TOOK THIS AWAY TO TEST IF IT RESOLVED THE ISSUES.
@@ -58,6 +58,7 @@ async function connectDISTO() {
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('connectBtn').addEventListener('click', connectDISTO);
 });
+
 
 
 
